3c1e2892001c15053511c23a1d9c69d6971d0aff
francivaldonlima <francivaldonlima@gmail.com>
2024-08-17 17:02:40 -0300

    criando footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import React from 'react';
+import Footer from './components/footer';
 
-import './App.css'
-
-function App() {
- 
-
+const App: React.FC = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <div className="min-h-screen flex flex-col">
+      {/* Outras partes da aplicação */}
+      <main className="flex-grow">
+        {/* Conteúdo principal */}
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/footer.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FaDiscord, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import Logo_HS from '../assets/Logo_HS.svg';
+
+
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-purple-900 text-white py-8">
+            <div className="container mx-auto flex flex-col items-center">
+            <div className="container mx-auto flex flex-col items-center">
+            <div className="md:flex-grow md:justify-start">
+                    <img src={Logo_HS} alt="Logo" className="h-16 md:flex-grow md:justify-start" />
+                </div>
+
+
+                </div>
+                {/* Logo Logo_HS */}
+
+
+                {/* Redes Sociais */}
+                <div className="mb-4 text-center">
+                    <h2 className="text-lg mb-2">Redes sociais:</h2>
+                    <div className="flex space-x-8 justify-center">
+                        <div className="flex flex-col items-center">
+                            <FaDiscord className="h-8 w-8 mb-1" />
+                            <p>#HelxSiena1734</p>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <FaInstagram className="h-8 w-8 mb-1" />
+                            <p>@helxsiena</p>
+                        </div>
+                        <div className="flex flex-col items-center">
+                            <FaLinkedin className="h-8 w-8 mb-1" />
+                            <p>Helena Siena</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Linha divisória */}
+                <div className="w-full border-t border-yellow-500 mt-4"></div>
+
+                {/* Direitos reservados */}
+                <div className="mt-4 text-center">
+                    <p className="text-sm">
+                        <span className="mr-1">©</span>
+                        Helena Siena - Todos os direitos reservados - 2023
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
